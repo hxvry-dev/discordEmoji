76260a9621bc35efb59c24b81cf951a0bb76123a
Henry <72309050+hxvry-dev@users.noreply.github.com>
2022-09-19 17:56:37 -0400

    ugh

--- .beta/emoji.py
+++ .beta/emoji.py
@@ -2,3 +2,10 @@ import csv
 import datetime as dt
 import colorama
 import textwrap
+
+ts = dt.datetime.now()
+date = ts.strftime("%m%d%Y")
+time = ts.strftime("%H%M%S")
+print(date + " " + time)
+
+# Using writelines to programatically generate outputs
