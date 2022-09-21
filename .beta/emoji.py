12ad9f31cbaebe535e984c42f7a9f3a464b60c74
Henry <72309050+hxvry-dev@users.noreply.github.com>
2022-09-21 01:40:20 -0400

    lol

--- .beta/emoji.py
+++ .beta/emoji.py
@@ -6,23 +6,28 @@ sp = '-----'
 console = Fore.LIGHTBLACK_EX + '[CONSOLE]  ' + Fore.RESET
 info = Fore.BLUE + '[INFO]  ' + Fore.RESET
 class Emoji:
-  a = Airium()
   def __init__(self):
-    self.lines = ''
-    self.emoji = self.getEmoji()
-    
-  def getEmoji(self):
-    return input(console + "Name of the Emoji as it appears in Discord >> ")
+    self.name = input(console + "Name of the Emoji as it appears in Discord >> ")
+    self._t = dt.datetime.now().strftime('%Y%m%d%H%M%S')
 
-  def wFile(self):
-    ts = dt.datetime.now()
-    t = ts.strftime('%Y%m%d%H%M%S')
-    date = ts.strftime("%m%d%Y")
-    time = ts.strftime("%H%M%S")
+  def basic_body(self):
+    f = open('./emoji/' + self._t + '-' + self.name + '.html', 'w')
+    a = Airium()
+    html = str(a)
+    a('<!DOCTYPE html>')
+    with a.html(lang='en'):
+      with a.head():
+        a.meta(charset='utf-8')
+        a.meta(content='width=device-width, initial-scale=1', name='viewport')
+        a.link(href='./content.css', rel='stylesheet')
+        a.title(_t=f'Welcome to the emojiMaker v2.0!')
+    f.write(html)
+    f.close()
 
 def main():
   e = Emoji()
-  e.wFile()
+  e.basic_body()
+
 
 if __name__ == '__main__':
     main()
