dc6c5f978a7cd3936b82b97d196b38d15f45eabd
Henry <72309050+hxvry-dev@users.noreply.github.com>
2022-09-21 00:50:19 -0400

    airium

--- .beta/emoji.py
+++ .beta/emoji.py
@@ -1,38 +1,28 @@
 import datetime as dt
 from colorama import Fore
-import re
+from airium import Airium
 
 sp = '-----'
 console = Fore.LIGHTBLACK_EX + '[CONSOLE]  ' + Fore.RESET
 info = Fore.BLUE + '[INFO]  ' + Fore.RESET
 class Emoji:
-    def __init__(self):
-        self.lines = ''
-        self.emoji = self.getEmoji()
+  a = Airium()
+  def __init__(self):
+    self.lines = ''
+    self.emoji = self.getEmoji()
     
-    def getEmoji(self):
-        return input(console + "Name of the Emoji as it appears in Discord >> ")
+  def getEmoji(self):
+    return input(console + "Name of the Emoji as it appears in Discord >> ")
 
-    def wFile(self):
-        ts = dt.datetime.now()
-        t = ts.strftime('%Y%m%d%H%M%S')
-        date = ts.strftime("%m%d%Y")
-        time = ts.strftime("%H%M%S")
-        head = '''<!DOCTYPE html>
-<html>
-  <head>
-    <title>Emoji Result</title>
-  </head>
-  <body>
-  <h1 style="padding-left: 5px">Welcome to the EmojiMaker v2.0!</h1></br>
-    <h1 style="padding-left: 15px">{Idea}</h1>
-  </body>
-</html>'''
-        with open('./emoji/' + t + "-" + self.emoji + '.html', 'w') as f:
-            f.write(head)
+  def wFile(self):
+    ts = dt.datetime.now()
+    t = ts.strftime('%Y%m%d%H%M%S')
+    date = ts.strftime("%m%d%Y")
+    time = ts.strftime("%H%M%S")
 
 def main():
-    e = Emoji()
-    e.wFile()
+  e = Emoji()
+  e.wFile()
+
 if __name__ == '__main__':
     main()
