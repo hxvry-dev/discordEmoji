1cc8840c814ffe79657d9c02422499c31ab53ab2
Henry <72309050+hxvry-dev@users.noreply.github.com>
2022-09-21 23:28:38 -0400

    gaming

--- .beta/emoji.py
+++ .beta/emoji.py
@@ -7,11 +7,12 @@ console = Fore.LIGHTBLACK_EX + '[CONSOLE]  ' + Fore.RESET
 info = Fore.BLUE + '[INFO]  ' + Fore.RESET
 class Emoji:
   def __init__(self):
-    self._t = dt.datetime.now().strftime('%Y%m%d%H%M%S')
-    self.name = input(console + "Name of the Emoji as it appears in Discord >> ")
+    self.t = dt.datetime.now().strftime('%Y%m%d%H%M%S')
+    #Commenting this out for easy running of the file
+    #self.name = input(console + "Name of the Emoji as it appears in Discord >> ")
 
-  def basic_body(self):
-    f = open('./emoji/' + self._t + '-' + self.name + '--dev' + '.html', 'w')
+  def basic_body(self):          # '-' + self.name + 
+    f = open('./emoji/' + self.t + '--dev' + '-' +  dt.datetime.now().strftime('%M%S') + '.html', 'w')
     a = Airium()
     a('<!DOCTYPE html>')
     with a.html(lang='en'):
@@ -22,11 +23,9 @@ class Emoji:
         a.title(_t=f'Welcome to the emojiMaker v2.0!')
       with a.body():
         with a.div(id='emoji-name-container'):
-          a.img(src='../emoji/img/1.png')
           with a.span(id='text-container'):
-            a.p('This is your emoji as it appears in the Discord Emoji Search bar > ')
-            with a.code():
-              a(self.name)
+            with a.p(_t='This is your emoji as it appears in the Discord Emoji Search bar > '):
+              a.code(_t=self.t)
 
     html = str(a)
     f.write(html)
